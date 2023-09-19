796859548692a834738b72307fe6b1b3dbb873aa
fr7nz1 <fr7nz1@outlook.com>
2023-09-20 00:57:04 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String regex = "#([a-fA-F_0-9]{6})";
+        String str = "";
+
+        while (true)
+        {
+            System.out.print("Введите цвет HEX: ");
+            Scanner sc = new Scanner(System.in);
+            try {
+                str = sc.nextLine();
+                break;
+            } catch (Exception e) {
+                System.out.print("Ошибка!\n");
+            }
+        }
+
+        Pattern p1 = Pattern. compile (regex);
+        Matcher m1 = p1.matcher(str);
+        while (m1.find()) {
+            System.out.println("HEX: " + m1.group());
+        }
+    }
+}
